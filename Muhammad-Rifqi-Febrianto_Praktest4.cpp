cc20ee886b0bc001afbd7d0455679e0b97cd7bb2
rfq21 <112568065+rfq21@users.noreply.github.com>
2022-09-29 13:14:38 +0700

    Add files via upload

---
+++ Muhammad-Rifqi-Febrianto_Praktest4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	int x;
+	cin >> x;
+	int n;
+	cin >> n;
+	
+	for(int i = x;i <= n; i=i+x)
+	{
+	
+			cout << i << endl;
+		
+	}
+
+	
+
+}
